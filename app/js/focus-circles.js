cb70599871862669f941019fd7b2550e5b72730a
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2020-04-04 18:57:06 -0600

    Focus section: Created a script to animate shift of height between content but using translation instead of height to keep it smooth.

--- app/js/focus-circles.js
+++ app/js/focus-circles.js
@@ -1,6 +1,8 @@
 var Circles = function() {
 	var _self = this;
 
+
+	// Circles interaction
 	this.index = 0;
 	this.blocks = document.getElementsByClassName('focus-info-block');
 	this.triggers = document.getElementsByClassName('graph-circle');
@@ -8,8 +10,27 @@ var Circles = function() {
 	this.circlesContainer = document.getElementById('graph-circles-container');
 	this.transitionDelay = 700; // time in ms
 
+
+	// Text blocks animation
+	this.container = document.getElementById('focus-block_text-container');
+	this.displacementTarget = document.getElementById('graph-circles-container');
+	
+	this.heights = [];
+	this.currentHeight = 0;
+	this.maxHeight = 0;
+	
+	this.displacement = 0;
+
+
 	this.init = function() {
 		this.addListeners();
+
+		// Text blocks animation
+		this.getHeights();
+		this.getMaxHeight();
+		this.setHeight();
+
+		this.shiftBlocks(0);
 	}
 
 	this.addListeners = function() {
@@ -32,6 +53,9 @@ var Circles = function() {
 				_self.shiftCircles();
 			});
 		}
+
+		// Text blocks animation
+		window.addEventListener('resize', debounce(_self.resize, 500) );
 	}
 
 	this.shiftCircles = function(id) {
@@ -40,24 +64,32 @@ var Circles = function() {
 				_self.switchBlocks(1);
 				_self.switchIndex(1);
 				_self.switchCircles(0);
+
+				_self.shiftBlocks(1);
 				break;
 
 			case 'cps':
 				_self.switchBlocks(2);
 				_self.switchIndex(2);
 				_self.switchCircles(1);
+
+				_self.shiftBlocks(2);
 				break;
 
 			case 'sc':
 				_self.switchBlocks(3);
 				_self.switchIndex(3);
 				_self.switchCircles(2);
+
+				_self.shiftBlocks(3);
 				break;
 
 			default:
 				_self.switchBlocks(0);
 				_self.switchIndex(0);
 				_self.defaultCircles();
+
+				_self.shiftBlocks(0);
 		}
 	}
 
@@ -90,8 +122,64 @@ var Circles = function() {
 		this.circlesContainer.classList.remove('active');
 	}
 
+
 	this.switchIndex = function(index) {
 		this.index = index;
+	}
+
+	this.resize = function() {
+		_self.getHeights();
+		_self.getMaxHeight();
+		_self.setHeight();
+
+		_self.shiftBlocks(_self.index);
+	}
+
+	this.getHeights = function() {
+		this.heights = [];
+
+		for (var i = 0; i < this.blocks.length; i++) {
+			var _height = this.blocks[i].getBoundingClientRect().height;
+
+			this.heights.push(_height);
+		}
+	}
+
+	this.getCurrentHeight = function() {
+		this.currentHeight = this.heights[this.index];
+	}
+
+	this.getMaxHeight = function() {
+		this.maxHeight = Math.max(...this.heights);
+	}
+
+	this.getDisplacement = function() {
+		if (window.innerWidth < 768) {
+			this.displacement = this.currentHeight - this.maxHeight;
+		} else {
+			this.displacement = 0;
+		}
+	}
+
+	this.setHeight = function() {
+		if (window.innerWidth < 768) {
+			this.container.style.height = this.maxHeight + "px";
+		} else {
+			this.container.style.height = "";
+		}
+	}
+	
+
+	this.translate = function() {
+		this.displacementTarget.style.transform = "translate3d(0, " + this.displacement + "px, 0)";
+	}
+
+	this.shiftBlocks = function(index) {
+		this.index = index;
+
+		this.getCurrentHeight();
+		this.getDisplacement();
+		this.translate();
 	}
 }
 
