6a03b78c59826c28597db4182462df760260f682
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2020-06-01 12:22:12 -0500

    General UX: inview animations triggered right before preloader hides.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -3,7 +3,7 @@ var PageSmoothScroll;
 var clickScroll;
 var smoothScrollWindowMinWidth = 1025;
 var inviewTriggerInSmoothScroll = false;
-var pageInitDelay;
+var pageInitDelay = 0;
 
 
 
@@ -47,54 +47,6 @@ window.onload = function() {
         pageInitDelay = PageSmoothScroll.state.initDelay;
     }
 
-    // Landing inview animation, linked with "Cascading" system
-    var inviewObjects = document.getElementsByClassName('motion-cascade');
-
-    // Cascade animation timing values
-    for (var i = 0; i < inviewObjects.length; i++) {
-        var inview = InView(inviewObjects[i], function(isInView, data) {
-            if ((this.el.getBoundingClientRect().top - window.innerHeight) < 0) {
-                this.el.classList.add('animate');
-
-                if(this.el.querySelector('#services-animation')) {
-                    this.el.querySelector('#services-animation').classList.add('drawn');
-                }
-
-                if(this.el.querySelector('#blog-animation')) {
-                    this.el.querySelector('#blog-animation').classList.add('drawn');
-                }
-
-                if(this.el.querySelector('#talks-animation')) {
-                    this.el.querySelector('#talks-animation').classList.add('drawn');
-                }
-
-                if(this.el.querySelector('#help-animation')) {
-                    this.el.querySelector('#help-animation').classList.add('drawn');
-                }
-
-            } else {
-                this.el.classList.remove('animate');
-
-                if(this.el.querySelector('#services-animation')) {
-                    this.el.querySelector('#services-animation').classList.remove('drawn');
-                }
-
-                if(this.el.querySelector('#blog-animation')) {
-                    this.el.querySelector('#blog-animation').classList.remove('drawn');
-                }
-
-                if(this.el.querySelector('#talks-animation')) {
-                    this.el.querySelector('#talks-animation').classList.remove('drawn');
-                }
-
-                if(this.el.querySelector('#help-animation')) {
-                    this.el.querySelector('#help-animation').classList.remove('drawn');
-                }
-
-            }
-        })
-    }
-
 
     if (window.location.pathname == "/" || window.location.pathname == "/index.html" || window.location.pathname == "index") {
         // Circles in "Focus" section interaction
@@ -134,7 +86,58 @@ window.onload = function() {
     }
 
     setTimeout(function(){
+        // Inview objects animation, linked with "Cascading" system
+        var inviewObjects = document.getElementsByClassName('motion-cascade');
+
+        // Cascade animation timing values
+        for (var i = 0; i < inviewObjects.length; i++) {
+            var inview = InView(inviewObjects[i], function(isInView, data) {
+                if ((this.el.getBoundingClientRect().top - window.innerHeight) < 0) {
+                    this.el.classList.add('animate');
+
+                    if(this.el.querySelector('#services-animation')) {
+                        this.el.querySelector('#services-animation').classList.add('drawn');
+                    }
+
+                    if(this.el.querySelector('#blog-animation')) {
+                        this.el.querySelector('#blog-animation').classList.add('drawn');
+                    }
+
+                    if(this.el.querySelector('#talks-animation')) {
+                        this.el.querySelector('#talks-animation').classList.add('drawn');
+                    }
+
+                    if(this.el.querySelector('#help-animation')) {
+                        this.el.querySelector('#help-animation').classList.add('drawn');
+                    }
+
+                } else {
+                    this.el.classList.remove('animate');
+
+                    if(this.el.querySelector('#services-animation')) {
+                        this.el.querySelector('#services-animation').classList.remove('drawn');
+                    }
+
+                    if(this.el.querySelector('#blog-animation')) {
+                        this.el.querySelector('#blog-animation').classList.remove('drawn');
+                    }
+
+                    if(this.el.querySelector('#talks-animation')) {
+                        this.el.querySelector('#talks-animation').classList.remove('drawn');
+                    }
+
+                    if(this.el.querySelector('#help-animation')) {
+                        this.el.querySelector('#help-animation').classList.remove('drawn');
+                    }
+
+                }
+            })
+        }
+
+
+        // Remove preloader
         Preloader.init();
+
     }, pageInitDelay);
 
 }
